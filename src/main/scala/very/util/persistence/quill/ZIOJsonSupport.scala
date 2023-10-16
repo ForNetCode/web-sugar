6d17811a819cb379756790fb4262b9c6e0a6ccfe
timzaak <zsy.evan@gmail.com>
2023-10-16 09:56:16 +0800

    add quill zio json decoder\encoder support

--- src/main/scala/very/util/persistence/quill/ZIOJsonSupport.scala
+++ src/main/scala/very/util/persistence/quill/ZIOJsonSupport.scala
@@ -4,12 +4,13 @@ import io.getquill.context.jdbc.JdbcContextTypes
 import io.getquill.PostgresDialect
 import org.postgresql.util.PGobject
 import zio.json.*
+import zio.json.ast.Json
 
 trait DBSerializer
 trait ZIOJsonSupport {
   this: JdbcContextTypes[PostgresDialect, _] =>
 
-  given encodeJsonb[T<:DBSerializer](using JsonEncoder[T]): Encoder[T] = {
+  given encodeJsonb[T <: DBSerializer](using JsonEncoder[T]): Encoder[T] = {
     encoder(
       java.sql.Types.OTHER,
       (index, value, row) => {
@@ -20,10 +21,28 @@ trait ZIOJsonSupport {
       }
     )
   }
+  given encodeJson: Encoder[Json] = {
+    encoder(
+      java.sql.Types.OTHER,
+      (index, value, row) => {
+        val jsonObject = new PGobject()
+        jsonObject.setType("jsonb")
+        jsonObject.setValue(value.toJson)
+        row.setObject(index, jsonObject)
 
-  given decodeJsonb[T<:DBSerializer](using JsonDecoder[T]):Decoder[T] =
-    decoder{(index,row, session) =>
+      }
+    )
+  }
+
+  given decodeJsonb[T <: DBSerializer](using JsonDecoder[T]): Decoder[T] =
+    decoder { (index, row, session) =>
       val data = row.getString(index)
       data.fromJson[T].toOption.get
+    }
+
+  given decodeJson: Decoder[Json] =
+    decoder { (index, row, session) =>
+      val data = row.getString(index)
+      data.fromJson[Json].toOption.get
     }
 }
