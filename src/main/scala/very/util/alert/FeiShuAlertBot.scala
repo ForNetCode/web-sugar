887ce4eec1b90994fb89110cf78d0e4f5b0e94a4
timzaak <zsy.evan@gmail.com>
2023-11-14 15:23:57 +0800

    add log alert for mock

--- src/main/scala/very/util/alert/FeiShuAlertBot.scala
+++ src/main/scala/very/util/alert/FeiShuAlertBot.scala
@@ -8,14 +8,15 @@ import very.util.config.WithConfig
 import zio.json.*
 import zio.json.ast.{ Json, JsonCursor }
 
-//TODO: support send with secret
 
 trait WithAlert extends WithConfig {
   import very.util.config.getOptional
   object feiShuAlertBot
     extends FeiShuAlertBot(config.getString("alert.feishu.url"), config.getOptional[String]("alert.feishu.url.secret"))
 }
-class FeiShuAlertBot(url: String, secret: Option[String] = None) {
+
+//TODO: support send with secret
+class FeiShuAlertBot(url: String, secret: Option[String] = None) extends Alert {
 
   private val client =
     Slf4jLoggingBackend(HttpClientSyncBackend())
