ddd316988a20c826d1953a0faf1162793206c1a0
timzaak <zsy.evan@gmail.com>
2024-01-02 14:56:47 +0800

    extract jsonb function

--- src/main/scala/very/util/persistence/quill/CirceJsonSupport.scala
+++ src/main/scala/very/util/persistence/quill/CirceJsonSupport.scala
@@ -12,17 +12,17 @@ trait CirceJsonSupport {
 
   private val jsonPrinter: Printer = Printer.noSpaces.copy(dropNullValues = true)
 
-  given encodeJsonb[T <: DBSerializer](using encode: io.circe.Encoder[T]): Encoder[T] = {
-    encoder[T](
-      java.sql.Types.OTHER,
-      (index, value, row) => {
-        val jsonObject = new PGobject()
-        jsonObject.setType("jsonb")
-        jsonObject.setValue(jsonPrinter.print(encode(value)))
-        row.setObject(index, jsonObject)
-      }
-    )
-  }
+  protected def encodeEntityToJsonb[T](using encode: io.circe.Encoder[T]): Encoder[T] = encoder[T](
+    java.sql.Types.OTHER,
+    (index, value, row) => {
+      val jsonObject = new PGobject()
+      jsonObject.setType("jsonb")
+      jsonObject.setValue(jsonPrinter.print(encode(value)))
+      row.setObject(index, jsonObject)
+    }
+  )
+
+  given encodeJsonb[T <: DBSerializer](using encode: io.circe.Encoder[T]): Encoder[T] = encodeEntityToJsonb
 
   given encodeJson: Encoder[Json] = {
     encoder(
@@ -36,11 +36,14 @@ trait CirceJsonSupport {
     )
   }
 
-  given decodeJsonb[T <: DBSerializer](using io.circe.Decoder[T]): Decoder[T] =
+  protected def decodeEntityToJsonb[T](using io.circe.Decoder[T]): Decoder[T] =
     decoder { (index, row, session) =>
       val data = row.getString(index)
       decode(data).toOption.get
     }
+  given decodeJsonb[T <: DBSerializer](using io.circe.Decoder[T]): Decoder[T] =
+    decodeEntityToJsonb
+
 
   given decodeJson: Decoder[Json] =
     decoder { (index, row, session) =>
